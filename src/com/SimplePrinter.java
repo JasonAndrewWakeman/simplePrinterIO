eefc6f82a7691bd6b714373ff2817e6bb873e4f0
jwakeman-ebsco <jwakeman@ebsco.com>
2020-03-06 16:34:46 -0500

    adding while loop

--- src/com/SimplePrinter.java
+++ src/com/SimplePrinter.java
@@ -1,14 +1,38 @@
 package com;
 
+import java.util.Scanner;
+
 public class SimplePrinter {
 
     /**
      * @param args
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.println("Hello world!");
+        {
+
+            System.out.println("I am a printer.");
+            System.out.println("initialized");
+            Scanner scan = new Scanner(System.in);
+
+            String input;
+            do
+            {
+                System.out.println("Press 1 to print, 2 to add ink, 3 to add paper");
+                input = scan.next();
+                if(input.equals("1"))
+                {
+                    System.out.println("You pressed 1");
+                } else if (input.equals("2")){
+                System.out.println("You pressed 2");
+                } else if (input.equals("3")){
+                System.out.println("You pressed 3");
+                }
+                else {
+                System.out.println("invalid input");
+            }
+            }
+            while (!input.equals("enter") || !input.equals("Enter") || !input.equals("leave") || !input.equals("Leave"));
+
+        }
     }
-
-
 }

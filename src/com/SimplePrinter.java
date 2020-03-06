96e6e4b1b3fabae61f6f6dedc347278a1b2079e4
jwakeman-ebsco <jwakeman@ebsco.com>
2020-03-06 16:17:52 -0500

    adding first file

---
+++ src/com/SimplePrinter.java
@@ -0,0 +1,14 @@
+package com;
+
+public class SimplePrinter {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello world!");
+    }
+
+
+}
